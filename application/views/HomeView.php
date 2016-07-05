d986040c0baad2b270962b1b2c88f354b996e45a
akashakram <akashakram@outlook.com>
2016-07-05 11:48:59 +0600

    Commit 3: Grid View added.

--- application/views/HomeView.php
+++ application/views/HomeView.php
@@ -1,15 +1,18 @@
 
 <style type="text/css">
-	
+	h3, td{
+		text-align: right;
+	}
 </style>
-
-<div class="col-md-6">
-
-	<table  class="table">
-			
+<div class=""></div>
+<div class="col-md-12">
+	<div class="row">
 		<?php foreach ($content as $user): ?>
+	<div class="col-md-6">
+		<table class="table table-hover">
+			
 		<tr>
-			<th><h2><?php echo $user->name ?></h2></th>
+			<th colspan="2"><h3><?php echo $user->name ?></h3></th>
 		</tr>
 		<tr>
 			<th>Email</th>
@@ -24,14 +27,16 @@
 			<td><?php echo $user->contactNo ?></td>
 		</tr>
 		<tr>
-			<td colspan="2" align="center">
+			<td colspan="2">
 				<a class="btn btn-success"   href="<?php echo base_url(); ?>view/<?php echo $user->id; ?>">View Profile</a>
 				<a class="btn btn-primary"  href="<?php echo base_url(); ?>modify/<?php echo $user->id; ?>">Modify</a>
 			    <a class="btn btn-danger"   href="<?php echo base_url(); ?>remove/<?php echo $user->id; ?>">Remove</a>
 			</td>
 		</tr>
+		</table>
+	</div>
 		 <?php endforeach; ?> 
-	</table>
+	</div>
 		
 </div>
 
